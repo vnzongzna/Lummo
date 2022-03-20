469c91be64371c5ec2b37c5a860d72cacffd43f8
vnzongzna <github@vaibhavk.in>
2022-03-20 16:49:08 +0530

    add prometheus metrics

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM golang:1.17-alpine AS build
 WORKDIR /src/
 COPY main.go /src/
 COPY go.* /src/
-COPY kv /src/kv
+COPY pkg /src/pkg
 RUN  CGO_ENABLED=0 go build -o /bin/server
 
 FROM scratch
--- main.go
+++ main.go
@@ -6,7 +6,9 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	kv "github.com/vnzongzna/lummo/kv/v1"
+
+	kv "github.com/vnzongzna/lummo/pkg/kv/v1"
+	"github.com/vnzongzna/lummo/pkg/metrics"
 )
 
 func main() {
@@ -15,10 +17,12 @@ func main() {
 
 	r.Use(middleware.Logger)
 	r.Use(middleware.AllowContentType("application/json"))
+	r.Use(metrics.PrometheusMiddleware)
 
 	r.Get("/get/{key}", kv.Get)
 	r.Post("/set", kv.Set)
 	r.Get("/search", kv.Search)
+	r.Handle("/prometheus", metrics.Handler())
 
 	log.Fatal(http.ListenAndServe(":8088", r))
 }
